7b8ff5fc2735ffcff3fb448aca7c5c3ffbd49324
Elyse Kriegel <esamkriegel@gmail.com>
2025-03-28 22:50:05 -0500

    firsst commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+/** @type {import('vite').UserConfig} */
+import { resolve } from 'path'
+import { Result } from 'postcss';
+export default {
+  //absolute path
+  base: '/394-portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'pages/contact.html'),
+        essay: resolve(__dirname, 'pages/essay.html'),
+        works: resolve(__dirname, 'pages/works.html'),
+        seniordesign: resolve(__dirname, 'pages/seniordesign.html'),
+        objectives: resolve(__dirname, 'pages/objectives.html')
+      },
+    },
+  }
+};
